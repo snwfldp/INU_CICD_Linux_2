a8782335094114366867012ae260ccfce0cfefcf
snwfld <sdbrandon040810@naver.com>
2024-12-07 02:33:18 +0900

    errorcheck_2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1> Incheon National University !!</h1>");
+    res.send("<h1> Incheon National University !!!</h1>");
 });
 
 app.listen(port, () => {
